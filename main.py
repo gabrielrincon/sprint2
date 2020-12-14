2d00ccb05c7515318574c3402ee8b14db0b50ade
Gabriel Rincón <anivelmundial@hotmail.com>
2020-12-13 22:26:44 -0500

    + correccion final por probar 3

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from db.accounts_db import AccountInDB, get_account_db, create_account_db
+from db.accounts_db import get_account_db, create_account_db
 
 from models.account_models import AccountIn, AccountOut
 
 from fastapi import FastAPI
 from fastapi import HTTPException
 
+api = FastAPI()
 
-app = FastAPI()
-
-
-# @app.post("/user/auth/")
-# async def auth_user(user_in: UserIn):
-#     user_in_db = get_user(user_in.username)
-#     if user_in_db == None:
-#         raise HTTPException(status_code=404,
-#                             detail="El usuario no existe")
-#     if user_in_db.password != user_in.password:
-#         return {"Autenticado": False}
-#     return {"Autenticado": True}
-#
-
-@app.post("/account/create/")
+@api.post("/account/create/")
 async def create_account(account_in: AccountIn):
     result = create_account_db(account_in)
     print(result)
@@ -29,7 +16,7 @@ async def create_account(account_in: AccountIn):
     else:
         raise HTTPException(status_code=404, detail="La cuenta ya fue creada")
 
-@app.get("/account/get/{name}")
+@api.get("/account/get/{name}")
 async def get_account(name: str):
     account_in_db = get_account_db(name)
     if account_in_db == None:
